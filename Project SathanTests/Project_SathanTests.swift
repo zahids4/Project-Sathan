88cb257b82512893dfc8dd7bc1465df08d8ec80f
Saim Zahid <saimzahid@me.com>
2018-11-11 21:52:02 -0500

    Initial Commit.

---
+++ Project SathanTests/Project_SathanTests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_SathanTests.swift
+//  Project SathanTests
+//
+//  Created by Saim Zahid on 2018-11-11.
+//  Copyright © 2018 Saim Zahid. All rights reserved.
+//
+
+import XCTest
+@testable import Project_Sathan
+
+class Project_SathanTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
